d5a55f349e0c018ef934242ea7ccd81c9108cda8
Ivan Dustin B. Bilon <ivan22.dust@gmail.com>
2021-03-29 11:51:13 +0800

    Test cumulative book letters

--- test/tests.js
+++ test/tests.js
@@ -400,3 +400,26 @@ describe('book total value', function() {
         })
     })
 })
+
+describe('cumulative book letters', function() {
+    it('is correct', function() {
+        let actual   = 0
+        let expected = 0
+
+        books.forEach(function(book, index) {
+            book.chapters.forEach(function(chapter) {
+                chapter.verses.forEach(function(verse) {
+                    verse.words.forEach(function(word) {
+                        if (omit(word))
+                            return
+
+                        actual    = word.cumulative_book_letters
+                        expected += word.letters
+                    })
+                })
+            })
+        })
+
+        assert.equal(actual, expected)
+    })
+})
